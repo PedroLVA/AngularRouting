3853b4e697725f287ab298f5201e9f5a651c4d8f
Pedro Athayde <pedro_athayde@outlook.com>
2024-07-23 16:43:03 -0300

    Added logic to sort tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { Component, computed, DestroyRef, inject, input, OnInit, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
@@ -16,15 +16,23 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent implements OnInit {
   private tasksService = inject(TasksService)
   //order = input<'asc' | 'desc'>();
-  order?: 'asc' | 'desc'
+  order = signal<'asc' | 'desc'>('desc')
   userId = input.required<string>()
-  userTasks = computed(() => this.tasksService.allTasks().filter(task => task.userId === this.userId()))
+  userTasks = computed(() => this.tasksService.allTasks().filter(task => task.userId === this.userId())
+  .sort((a, b) => {
+    if(this.order() === 'desc'){
+      return a.id > b.id ? -1 : 1
+    }else{
+      return a.id > b.id ? 1 : -1
+    }
+  })
+)
   private destroyRef = inject(DestroyRef);
   private activatedRoute = inject(ActivatedRoute)
 
   ngOnInit(): void {
     const subscription =this.activatedRoute.queryParams.subscribe({
-      next: params => this.order = params['order']
+      next: params => this.order.set(params['order']),
     })
     this.destroyRef.onDestroy(() => subscription.unsubscribe())
   }
