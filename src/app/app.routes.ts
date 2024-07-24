1d6b392a838931aaa6dc755867841e93a62073b2
Pedro Athayde <pedro_athayde@outlook.com>
2024-07-24 10:49:35 -0300

    added route guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) =>{
+    const router = inject(Router)
+    const shouldGetAcess = Math.random();
+    if(shouldGetAcess < 0.5){
+        return true;
+    }
+    return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes: Routes = [
     {
         path: '',
-        component: NoTaskComponent
+        component: NoTaskComponent,
+        title: 'No task selected'
     },
     {
         path: 'users/:userId',
@@ -30,6 +41,7 @@ export const routes: Routes = [
 
             },
         ],
+        canMatch: [dummyCanMatch],
         data: {
             message: 'hello!'
         },
